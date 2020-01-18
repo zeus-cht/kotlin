8a26e16c29ccf153404f24dba41fd0d1cd2b469c
Zalim Bashorov <zalim.bashorov@jetbrains.com>
2020-01-18 14:05:36 +0300

    ~ Char: add valueOf

--- libraries/stdlib/js-ir/builtins/Char.kt
+++ libraries/stdlib/js-ir/builtins/Char.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010-2018 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Copyright 2010-2020 JetBrains s.r.o. and Kotlin Programming Language contributors.
  * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
  */
 
@@ -55,6 +55,10 @@ public data class Char internal constructor(val value: Int) : Comparable<Char> {
     public override fun toString(): String {
         return js("String").fromCharCode(value).unsafeCast<String>()
     }
+
+    // TODO make Char inline class and remove it
+    @JsName("valueOf")
+    protected fun valueOf() = value
 
     companion object {
         /**
